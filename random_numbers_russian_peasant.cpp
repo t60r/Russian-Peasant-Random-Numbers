e17fd616bf610dc013b7781b719e4ac7c418bf2c
t60r <merkinmaker@gmail.com>
2014-03-20 11:05:40 -0500

    Removed all system pause statements.

--- random_numbers_russian_peasant.cpp
+++ random_numbers_russian_peasant.cpp
@@ -5,6 +5,9 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
 using namespace std;
 int main()
 
@@ -18,10 +21,8 @@ int main()
 	cin >> first;
 	cout << endl;
 	second = (rand()%100);//random mod 100
-	system("CLS");
 	cout << first << " Is a good number.  " << "I'll pick " << second << "." << endl;
-	system("PAUSE");
-	system("CLS");
+	while (getchar() != '\n');
 	cout << "\nNow it's time to show you how to do this.\n";
 	cout << "We will write two columns\n";
 	cout << "Our smallest number will go first\n";
@@ -32,24 +33,20 @@ int main()
 		second = temp;
 	}
 	cout << first << endl;
-	system("PAUSE");
-	system("CLS");
+	while (getchar() != '\n');
 	cout << "Our largest number will go second\n";
 	cout << first << "	" << second << endl;
-	system("PAUSE");
-	system("CLS");
+	while (getchar() != '\n');
 	cout << "Divide the first number by two, leaving out the remainder\n";
 	cout << "Write the result below the original\n";
 	cout << first << "	" << second << endl;
 	cout << (first/2) << endl;
-	system("PAUSE");
-	system("CLS");
+	while (getchar() != '\n');
 	cout << "Multiply the largest number by 2\n";
 	cout << "Write the result below\n";
 	cout << first << "	" << second << endl;
 	cout << (first/2) << "	" << (second*2)<< endl;
-	system("PAUSE");
-	system("CLS");
+	while (getchar() != '\n');
 	cout << "Continue doing this until the number in the bottom of the first column is '1'.\n";
 	//LOOP TO FILL ARRAYS WITH THE COLUMNS
 	const int COL = 7;//prevents going out of index
@@ -66,8 +63,7 @@ int main()
 		first = (first/2);
 		second = (second*2);
 	}
-	system("PAUSE");
-	system("CLS");
+	while (getchar() != '\n');
 	cout << "Find any odd numbers in the first column, and mark them.\n";
 	for (int i = 0; i < COL; ++i)
 	{
@@ -92,8 +88,7 @@ int main()
 		cout << col1[i] << "    " << col2[i] ;
 		cout << endl;
 	}
-	system("PAUSE");
-	system("CLS");
+	while (getchar() != '\n');
 	cout << "Find any odd numbers in the first column, and mark them.\n";
 	for (int i = 0; i < COL; ++i)
 	{
@@ -123,8 +118,7 @@ int main()
 	}
 	cout << "Finally, write out the hilighted numbers in the second row,\n";
 	cout << "and add them together.\n" << endl;;
-	system("PAUSE");
-	system("CLS");
+	while (getchar() != '\n');
 	//ADD INDEXES OF THE GOOD ARRAY
 	int answer = 0;
 	for (int i = 0; i<COL; ++i)
@@ -134,7 +128,7 @@ int main()
 	cout << "Thats it! The sum of the 'good' numbers from the second row\n";
 	cout << "equal the product of the two original numbers that you and I have made up!\n";
 	cout << "Our Final answer is " << answer << "!!!" << endl;
-	system("PAUSE");
-	int pause;
-	cin >> pause;
+	while (getchar() != '\n');
 }
+
+
